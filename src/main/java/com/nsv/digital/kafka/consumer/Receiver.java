1fe0195455184e25494b7dee890220c6a02b4bcd
nagarajsv3 <nagarajsv3@gmail.com>
2018-03-17 15:08:32 -0400

    Initial commit

---
+++ src/main/java/com/nsv/digital/kafka/consumer/Receiver.java
@@ -0,0 +1,26 @@
+package com.nsv.digital.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Receiver {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    @KafkaListener(topics = "${kafka.topic.c3topic}")
+    public void receive(String payload) {
+        LOGGER.info("received payload='{}'", payload);
+        latch.countDown();
+    }
+
+}
